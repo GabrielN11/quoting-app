90b4334d7f2c86e04638ead6eb374f11ad7d0eca
GabrielN11 <gabrielnunes11c@gmail.com>
2022-07-02 14:11:53 -0300

    adaptative icon fixed and screen glitch fixed

--- src/screens/Initial/Initial.jsx
+++ src/screens/Initial/Initial.jsx
@@ -8,28 +8,30 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { API_URL } from '../../../enviroment';
 
-const Initial = ({navigation}) => {
-    const {user, setUser} = React.useContext(GlobalContext)
+const Initial = ({ navigation }) => {
+    const { setUser } = React.useContext(GlobalContext)
     const [loading, setLoading] = React.useState(true)
     React.useEffect(() => {
         const fetchUser = async () => {
-            try{
+            try {
                 const token = await AsyncStorage.getItem('@user_token')
-                if(token){
+                if (token) {
                     const json = await fetch(API_URL + '/sign-in', {
-                        headers:{
+                        headers: {
                             'Authorization': 'Bearer ' + token
                         }
                     })
                     const response = await json.json()
                     response.data['token'] = token
                     setUser(response.data)
-                    navigation.navigate('Drawer')
-                    return
+                    setLoading(false)
+                    return navigation.reset({
+                        index: 0,
+                        routes: [{ name: 'Drawer' }],
+                    });
                 }
-            }catch(e){
+            } catch (e) {
                 console.log(e)
-            }finally{
                 setLoading(false)
             }
         }
@@ -37,8 +39,8 @@ const Initial = ({navigation}) => {
     }, [])
     return (
         <InitialView>
-            <SwiperComponent navigation={navigation}/>
-            {loading && <Loading/>}
+            <SwiperComponent navigation={navigation} />
+            {loading && <Loading />}
         </InitialView>
     );
 }
